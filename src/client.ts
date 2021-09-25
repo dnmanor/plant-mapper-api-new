81b240f5375dd8b20dd36a359e6e5a88043a6fe7
David Nuertey Manor <manornuertey@gmail.com>
2021-09-25 09:20:25 +0000

    add logging to client connection

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,18 @@
 const { Client } = require("pg");
+const log = require("./utilities/logger");
 
-module.exports = new Client({
+const options = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+};
+
+const connection = new Client(options);
+
+connection.on("error", (err: any, client: any) => {
+  log.error({ category: "DB", msg: err.message });
 });
+
+module.exports = connection;
