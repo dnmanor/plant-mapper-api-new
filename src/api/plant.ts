545e5339ae48f2d8a74f891ccb9feb16835544a4
David Nuertey Manor <manornuertey@gmail.com>
2021-09-27 08:33:06 +0000

    add util for plant search by name

--- src/api/plant.ts
+++ src/api/plant.ts
@@ -26,7 +26,25 @@ export async function getPlantsByCatergory(category: string) {
       values: [category],
     });
     if (response.rows.length === 0) {
-      throw new Error("Could not fetch plants");
+      throw new Error(`Could not fetch plants in ${category}`);
+    }
+    return response.rows;
+  } catch (error) {
+    log.error({ category: "BD", msg: error });
+  } finally {
+    await client.close();
+  }
+}
+
+export async function searchPlantByName(name: string) {
+  await client.connect();
+  try {
+    const response = await client.query({
+      text: `SELECT * FROM plants WHERE scientific_names || ' ' || common_names ILIKE '%$1%';`,
+      values: [name],
+    });
+    if (response.rows.length === 0) {
+      throw new Error(`Could not match any plants with the search term`);
     }
     return response.rows;
   } catch (error) {
