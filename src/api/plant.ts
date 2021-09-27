d003b76c3b81e69fe1e04d7871aeea30e52ec701
David Nuertey Manor <manornuertey@gmail.com>
2021-09-27 07:37:08 +0000

    add get plants by category util

--- src/api/plant.ts
+++ src/api/plant.ts
@@ -2,11 +2,29 @@ export {};
 const client = require("../client");
 const log = require("../utilities/logger");
 
-export default async function getAllPlants() {
+export async function getAllPlants() {
   await client.connect();
 
   try {
     const response = await client.query({ text: `SELECT * FROM plants` });
+    if (response.rows.length === 0) {
+      throw new Error("Could not fetch plants");
+    }
+    return response.rows;
+  } catch (error) {
+    log.error({ category: "BD", msg: error });
+  } finally {
+    await client.close();
+  }
+}
+
+export async function getPlantsByCatergory(category: string) {
+  await client.connect();
+  try {
+    const response = await client.query({
+      text: `SELECT * from plants WHERE category=$1`,
+      values: [category],
+    });
     if (response.rows.length === 0) {
       throw new Error("Could not fetch plants");
     }
