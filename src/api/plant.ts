0877d7cdc6b71fbbc2f028436da8ebc778413894
David Nuertey Manor <manornuertey@gmail.com>
2021-10-03 04:29:46 +0000

    add changes

--- src/api/plant.ts
+++ src/api/plant.ts
@@ -6,12 +6,24 @@ import {
 } from "../database/plant";
 
 module.exports = function (app: any) {
-  app.get("/plants", async (req, res) => {
-    try {
-      const plants = await getAllPlants();
-      res.status(200).send(plants);
-    } catch (error) {
-      res.status(400).send({ error });
+  app.get(
+    "/plants",
+    async (
+      req: any,
+      res: {
+        status: (arg0: number) => {
+          (): any;
+          new (): any;
+          send: { (arg0: { error: unknown }): void; new (): any };
+        };
+      }
+    ) => {
+      try {
+        const plants = await getAllPlants();
+        res.status(200).send(plants);
+      } catch (error) {
+        res.status(400).send({ error });
+      }
     }
-  });
+  );
 };
